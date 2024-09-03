5548a6f2e4720d7dd0f89a55419691d2d3a6443b
akshaypujari2507 <pujariakshayr2@gmail.com>
2024-09-03 10:03:19 +0530

    publishing changed

--- tmComponent/build.gradle.kts
+++ tmComponent/build.gradle.kts
@@ -8,6 +8,9 @@ plugins {
 
 }
 
+group = "com.example.publishcomponent"
+version = "0.1.0"
+
 kotlin {
     androidTarget {
         compilations.all {
@@ -57,4 +60,35 @@ android {
 dependencies {
     implementation(libs.androidx.runtime.android)
     implementation(libs.androidx.ui.graphics.android)
+}
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["kotlin"])
+            groupId = "com.example.publishcomponent"
+            artifactId = "PublishComponent"
+            version = "0.1.0"
+            // Add pom configuration if needed
+            //pom {
+              //  name.set("PublishComponent")
+                //description.set("A Kotlin Multiplatform Mobile Library")
+                //url.set("https://gitlab.com/ChandanaTawde/publishcomponent.git")
+            //}
+
+        }
+    }
+/*
+    repositories {
+        maven {
+            url = uri("https://gitlab.com/api/v4/projects/61331517/packages/maven")
+            credentials(HttpHeaderCredentials::class) {
+                name = "Private-Token"
+                value = System.getenv("glpat-QfFj6Pd8zyYe9pwPiy8K")
+            }
+            authentication {
+                create<HttpHeaderAuthentication>("header")
+            }
+        }
+    }
+*/
 }
